0a411526ec26a1401e3057f8cd1f858cf9958b1d
Mercedes Rodriguez <meche.grc@gmail.com>
2017-04-26 15:34:43 -0400

    amount edit text

---
+++ app/src/main/java/com/tekknow/bicentenario/tbcomplus/widget/AmountEditText.java
@@ -0,0 +1,52 @@
+package com.tekknow.bicentenario.tbcomplus.widget;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+
+/**
+ * Created by Mercedes Rodriguez on 4/26/2017.
+ */
+
+public class AmountEditText extends AppCompatEditText {
+
+    public AmountEditText(Context context) {
+        super(context);
+        setFocusBehavior();
+    }
+
+    public AmountEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusBehavior();
+    }
+
+    public AmountEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setFocusBehavior();
+    }
+
+    private void setFocusBehavior() {
+        setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    setText(getText().toString().replace(".", "").replace(",", "."));
+                } else {
+                    if (getText().length() > 0) {
+                        DecimalFormat formatter = new DecimalFormat();
+                        formatter.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(new Locale("es", "VE")));
+                        formatter.setDecimalSeparatorAlwaysShown(true);
+                        formatter.setMinimumFractionDigits(2);
+
+                        setText(formatter.format(Double.parseDouble(getText().toString())));
+                    }
+                }
+            }
+        });
+    }
+
+}
